19114bf5a44f74470c997864d708752dfbafcfde
Todor Lubenov <tlubenov@gmail.com>
2017-05-19 23:29:59 +0300

    set templates

--- tglserver/tglserver/settings.py
+++ tglserver/tglserver/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'tglserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates', '/home/dronekarta/tgdj_apps/tglserver/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
